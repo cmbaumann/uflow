07be83c2f06b39dd7ec095f6d64ad8c65482d508
brady wachs <bmwachs@crimson.ua.edu>
2021-09-27 10:25:44 -0500

    trying to add fill out elective
    problem may be with button type submit. But now not getting anything from the form

--- sourcecode/javascript/fill_out_elective.js
+++ sourcecode/javascript/fill_out_elective.js
@@ -9,11 +9,13 @@ function fill_elecitve() {
     var submit_button = document.getElementById("7Submit");
     var submit_info = document.getElementById("7ElectiveHelp");
     var edit_button = document.getElementById("7Edit");
-    
+
     submit_button.onclick = function() {
+        alert("first alert");
         submit_button.style.visibility = "hidden";
         submit_info.style.visibility = "hidden";
-        document.getElementById(course_id + "OutputElective").innerHTML = elective_name;
+        document.getElementById("7OutputElective").innerHTML = elective_name;
         edit_button.style.visibility = "visible";
+        alert('button has been clicked - after all instructions');
     };
 };
