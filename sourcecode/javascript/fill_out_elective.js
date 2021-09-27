ad25ecc709f5590c22d2f7dc07ad476fab97176e
brady wachs <bmwachs@crimson.ua.edu>
2021-09-26 22:06:39 -0500

    editing elective
    made individual boxes bigger to fit submission form. Working in js to be able to change what is displayed based on what is submitted

--- sourcecode/javascript/fill_out_elective.js
+++ sourcecode/javascript/fill_out_elective.js
@@ -1,4 +1,21 @@
-function fill_elecitve(course_id) {
-    var elective = document.getElementById(course_id);
-    var submit_button = document.getElementById(course_id + " submit");
-}
+function fill_elecitve() {
+    // temp fix 
+    // ultimately want to be able to pass course_id as arg from html file
+    var course_id = "7"
+    
+    var elective_box = document.getElementById(course_id + "Box");
+    var elective_textarea = document.getElementById("fillOutElective");
+    var elective_name = elective_textarea.value
+    var submit_button = document.getElementById(course_id + "Submit");
+    var submit_info = document.getElementById(course_id + "ElectiveHelp");
+    var edit_button = document.getElementById(course_id + "Edit");
+
+    edit_button.style.display = "none";
+
+    submit_button.onclick = function() {
+        submit_button.style.display = "none";
+        submit_info.style.display = "none";
+        document.getElementById(course_id + "OutputElective").innerHTML = elective_name;
+        edit_button.style.display = "block";
+    };
+};
