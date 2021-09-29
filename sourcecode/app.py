c032b7225ce84fc5782faacf939188f8ee9e0104
cmbaumann <55467404+cmbaumann@users.noreply.github.com>
2021-09-29 09:54:05 -0500

    separate flowchart.html into two files

--- sourcecode/app.py
+++ sourcecode/app.py
@@ -116,9 +116,10 @@ def logout():
     else:
         return render_template('index.html')
 
-@app.route("/flowchart/<new>", methods=['post', 'get'])
-def flowchart(new):
+@app.route("/flowchart-new", methods=['post', 'get'])
+def flowchart1():
     if request.method == "POST":
+        print("post")
         name = request.form.get("flowchartname")
         d1 = request.form.get("d1")
         d2 = request.form.get("d2")
@@ -157,7 +158,6 @@ def flowchart(new):
         d35 = request.form.get("d35")
         d36 = request.form.get("d36")
         d37 = request.form.get("d37")
-        save_new = request.form.get("newflowchart")
 
         #checking if name is found
         email = session["email"]
@@ -172,96 +172,152 @@ def flowchart(new):
                         break
                     else:
                         name_found = False
+        print("name_found: ", name_found)
 
         if (name == ""):
             message = 'Please enter a name for your flowchart'
-            return render_template('flowchart.html', new=True, message=message)
-        elif (name_found and new):
+            return render_template('flowchart-new.html', message=message)
+        elif (name_found):
             message = 'You already have a flowchart named ' + name
-            return render_template('flowchart.html', new=True, message=message)
+            return render_template('flowchart-new.html', message=message)
         else: 
+            print("writing to database")
             email = session["email"]
             len = 0
             entry = records.find({"email": email}, {"flowcharts": 1, "_id": 0})
             for item in entry:
                 for thing in item['flowcharts']:
                     len += 1
-            if (save_new == True):
-                records.update_one(
-                    {"email": email},
-                    {"$push": {
-                        "flowcharts": {
-                            "$each": [{ "name": name, 
-                                        "1": d1, "2": d2, "3": d3, "4": d4, "5": d5,
-                                        "6": d6, "7": d7, "8": d8, "9": d9, 
-                                        "10": d10, "11": d11, "12": d12, "13": d13,
-                                        "14": d14, "15": d15, "16": d16, "17": d17, "18": d18,
-                                        "19": d19, "20": d20, "21": d21, "22": d22, "23": d23,
-                                        "24": d24, "25": d25, "26": d26, "27": d27, "28": d28,
-                                        "29": d29, "30": d30, "31": d31, "32": d32, "33": d33,
-                                        "34": d34, "35": d35, "36": d36, "37": d37}],
-                            "$position": len
-                        }
+            print("length: ", len)
+            print("new entry")
+            records.update_one(
+                {"email": email},
+                {"$push": {
+                    "flowcharts": {
+                        "$each": [{ "name": name, 
+                            "1": d1, "2": d2, "3": d3, "4": d4, "5": d5,
+                            "6": d6, "7": d7, "8": d8, "9": d9, 
+                            "10": d10, "11": d11, "12": d12, "13": d13,
+                            "14": d14, "15": d15, "16": d16, "17": d17, "18": d18,
+                            "19": d19, "20": d20, "21": d21, "22": d22, "23": d23,
+                            "24": d24, "25": d25, "26": d26, "27": d27, "28": d28,
+                            "29": d29, "30": d30, "31": d31, "32": d32, "33": d33,
+                            "34": d34, "35": d35, "36": d36, "37": d37}],
+                        "$position": len
                     }
-                    })
-            else:
-                entry = records.find({"email": email}, {"flowcharts": 1, "_id": 0})
-                newData = []
-                len = 0
-                for item in entry:
-                    for thing in item['flowcharts']:
-                        len += 1
-                    for i in range(len):
-                        if (item['flowcharts'][i]["name"] == name):
-                            item['flowcharts'][i]["1"] = d1
-                            item['flowcharts'][i]["2"] = d2
-                            item['flowcharts'][i]["3"] = d3
-                            item['flowcharts'][i]["4"] = d4
-                            item['flowcharts'][i]["5"] = d5
-                            item['flowcharts'][i]["6"] = d5
-                            item['flowcharts'][i]["7"] = d6
-                            item['flowcharts'][i]["8"] = d7
-                            item['flowcharts'][i]["9"] = d8
-                            item['flowcharts'][i]["10"] = d9
-                            item['flowcharts'][i]["11"] = d10
-                            item['flowcharts'][i]["12"] = d11
-                            item['flowcharts'][i]["13"] = d12
-                            item['flowcharts'][i]["14"] = d13
-                            item['flowcharts'][i]["15"] = d14
-                            item['flowcharts'][i]["16"] = d15
-                            item['flowcharts'][i]["17"] = d16
-                            item['flowcharts'][i]["18"] = d17
-                            item['flowcharts'][i]["19"] = d18
-                            item['flowcharts'][i]["10"] = d19
-                            item['flowcharts'][i]["20"] = d20
-                            item['flowcharts'][i]["21"] = d21
-                            item['flowcharts'][i]["22"] = d22
-                            item['flowcharts'][i]["23"] = d23
-                            item['flowcharts'][i]["24"] = d24
-                            item['flowcharts'][i]["25"] = d25
-                            item['flowcharts'][i]["26"] = d26
-                            item['flowcharts'][i]["27"] = d27
-                            item['flowcharts'][i]["28"] = d28
-                            item['flowcharts'][i]["29"] = d29
-                            item['flowcharts'][i]["30"] = d30
-                            item['flowcharts'][i]["31"] = d31
-                            item['flowcharts'][i]["32"] = d32
-                            item['flowcharts'][i]["33"] = d33
-                            item['flowcharts'][i]["34"] = d34
-                            item['flowcharts'][i]["35"] = d35
-                            item['flowcharts'][i]["36"] = d36
-                            item['flowcharts'][i]["37"] = d37
-                        newData.append(item['flowcharts'][i])
+                }
+            })   
+            message = 'Your flowchart has been saved'
+            return redirect(url_for('flowchart2', name=name))#render_template('flowchart-edit.html', message=message, name=name)
+    if "email" in session: # GET
+        return render_template('flowchart-new.html')
+    else:
+        return redirect(url_for("login"))
 
+@app.route("/flowchart-edit/<name>", methods=['post', 'get'])
+def flowchart2(name):
+    if request.method == "POST":
+        print("post")
+        name = request.form.get("flowchartname")
+        d1 = request.form.get("d1")
+        d2 = request.form.get("d2")
+        d3 = request.form.get("d3")
+        d4 = request.form.get("d4")
+        d5 = request.form.get("d5")
+        d6 = request.form.get("d6")
+        d7 = request.form.get("d7")
+        d8 = request.form.get("d8")
+        d9 = request.form.get("d9")
+        d10 = request.form.get("d10")
+        d11 = request.form.get("d11")
+        d12 = request.form.get("d12")
+        d13 = request.form.get("d13")
+        d14 = request.form.get("d14")
+        d15 = request.form.get("d15")
+        d16 = request.form.get("d16")
+        d17 = request.form.get("d17")
+        d18 = request.form.get("d18")
+        d19 = request.form.get("d19")
+        d20 = request.form.get("d20")
+        d21 = request.form.get("d21")
+        d22 = request.form.get("d22")
+        d23 = request.form.get("d23")
+        d24 = request.form.get("d24")
+        d25 = request.form.get("d25")
+        d26 = request.form.get("d26")
+        d27 = request.form.get("d27")
+        d28 = request.form.get("d28")
+        d29 = request.form.get("d29")
+        d30 = request.form.get("d30")
+        d31 = request.form.get("d31")
+        d32 = request.form.get("d32")
+        d33 = request.form.get("d33")
+        d34 = request.form.get("d34")
+        d35 = request.form.get("d35")
+        d36 = request.form.get("d36")
+        d37 = request.form.get("d37")
+
+        print("writing to database")
+        email = session["email"]
+        len = 0
+        entry = records.find({"email": email}, {"flowcharts": 1, "_id": 0})
+        print("update entry")
+        entry = records.find({"email": email}, {"flowcharts": 1, "_id": 0})
+        newData = []
+        len = 0
+        for item in entry:
+            for thing in item['flowcharts']:
+                len += 1
+            for i in range(len):
+                if (item['flowcharts'][i]["name"] == name):
+                    item['flowcharts'][i]["1"] = d1
+                    item['flowcharts'][i]["2"] = d2
+                    item['flowcharts'][i]["3"] = d3
+                    item['flowcharts'][i]["4"] = d4
+                    item['flowcharts'][i]["5"] = d5
+                    item['flowcharts'][i]["6"] = d5
+                    item['flowcharts'][i]["7"] = d6
+                    item['flowcharts'][i]["8"] = d7
+                    item['flowcharts'][i]["9"] = d8
+                    item['flowcharts'][i]["10"] = d9
+                    item['flowcharts'][i]["11"] = d10
+                    item['flowcharts'][i]["12"] = d11
+                    item['flowcharts'][i]["13"] = d12
+                    item['flowcharts'][i]["14"] = d13
+                    item['flowcharts'][i]["15"] = d14
+                    item['flowcharts'][i]["16"] = d15
+                    item['flowcharts'][i]["17"] = d16
+                    item['flowcharts'][i]["18"] = d17
+                    item['flowcharts'][i]["19"] = d18
+                    item['flowcharts'][i]["10"] = d19
+                    item['flowcharts'][i]["20"] = d20
+                    item['flowcharts'][i]["21"] = d21
+                    item['flowcharts'][i]["22"] = d22
+                    item['flowcharts'][i]["23"] = d23
+                    item['flowcharts'][i]["24"] = d24
+                    item['flowcharts'][i]["25"] = d25
+                    item['flowcharts'][i]["26"] = d26
+                    item['flowcharts'][i]["27"] = d27
+                    item['flowcharts'][i]["28"] = d28
+                    item['flowcharts'][i]["29"] = d29
+                    item['flowcharts'][i]["30"] = d30
+                    item['flowcharts'][i]["31"] = d31
+                    item['flowcharts'][i]["32"] = d32
+                    item['flowcharts'][i]["33"] = d33
+                    item['flowcharts'][i]["34"] = d34
+                    item['flowcharts'][i]["35"] = d35
+                    item['flowcharts'][i]["36"] = d36
+                    item['flowcharts'][i]["37"] = d37
+                    newData.append(item['flowcharts'][i])
+                print(newData)
                 records.update_one(
                     {"email": email},
                     {"$set": {"flowcharts": newData}}
                 )
             message = 'Your flowchart has been saved'
-            new = False
-            return render_template('flowchart.html', message=message, new=False)
+            return render_template('flowchart-edit.html', message=message, name=name)
     if "email" in session: # GET
-        return render_template('flowchart.html', new=new)
+        return render_template('flowchart-edit.html')
     else:
         return redirect(url_for("login"))
 
