9746f4c04f1631035674467a10c46c8af54222e4
Michael McDavid <mmmcdavid1@crimson.ua.edu>
2021-10-17 20:23:11 -0500

    Added the ability to change colors of the course status options via presets. Changed how the page changes course color.

---
+++ sourcecode/static/flowchart.js
@@ -0,0 +1,118 @@
+window.onload = function() {
+    var x = document.getElementsByClassName("column");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.backgroundColor = "white";
+    }
+};
+
+    let curColor = "#ffffff";
+    let tak_col = "#00ff00";
+    let ipr_col = "#0000ff";
+    let fut_col = "#ff0000";
+    let curOption = "";
+
+    //Changes the colors used into various kinds of colorblind alternatives
+    function color_cb_opt(id) {
+        //console.log(id);
+        if (id == "0") {
+            tak_col = "#00ff00";
+            ipr_col = "#0000ff";
+            fut_col = "#ff0000";
+        }
+        else if (id == "1") {
+            tak_col = "#00ffff";
+            ipr_col = "#ff00ff";
+            fut_col = "#ffff00";
+
+        }
+        else if (id == "2") {
+            tak_col = "#000000";
+            ipr_col = "#5f5f5f";
+            fut_col = "#afafaf";
+
+        }
+        else if (id == "3") {
+            tak_col = "#0ffff0";
+            ipr_col = "#f00fff";
+            fut_col = "#fff00f";
+        }
+        if (curOption) {
+            if (curOption == "taken") {document.getElementById(curOption).style.backgroundColor=tak_col; curColor=tak_col;}
+            else if (curOption == "inprogress") {document.getElementById(curOption).style.backgroundColor=ipr_col; curColor=ipr_col;}
+            else if (curOption == "future") {document.getElementById(curOption).style.backgroundColor=fut_col; curColor=fut_col;}
+        }
+        for (var i=1; i<=37; i++) {
+            var newid = "d" + i.toString();
+            var curCourse = document.getElementById(newid).value;
+            if (curCourse == "taken") {document.getElementById(i).style.backgroundColor = tak_col;}
+            else if (curCourse == "inprogress") {document.getElementById(i).style.backgroundColor = ipr_col;}
+            else if (curCourse == "future") {document.getElementById(i).style.backgroundColor = fut_col;}
+        }
+    }
+
+    function color(id) {
+        if (id == curOption) {
+            curColor = "#ffffff"
+            curOption = "";
+            document.getElementById(id).style.backgroundColor=curColor;
+        }
+        else if (id == ("taken")) {
+            curColor = tak_col;
+            curOption = "taken";
+            document.getElementById(id).style.backgroundColor=curColor;
+            document.getElementById("inprogress").style.backgroundColor="white";
+            document.getElementById("future").style.backgroundColor="white";
+        }
+        else if (id == "inprogress") {
+            curColor = ipr_col;
+            curOption = "inprogress";
+            document.getElementById(id).style.backgroundColor=curColor;
+            document.getElementById("taken").style.backgroundColor="white";
+            document.getElementById("future").style.backgroundColor="white";
+        }
+        else if (id == "future") {
+            curColor = fut_col;
+            curOption = "future";
+            document.getElementById(id).style.backgroundColor=curColor;
+            document.getElementById("taken").style.backgroundColor="white";
+            document.getElementById("inprogress").style.backgroundColor="white";
+        }
+    }
+
+    function change(id) {
+        var id_col = document.getElementById(id).style.backgroundColor;
+        // newid = "picker c" + id
+        // if (curColor == "green") {
+        //     document.getElementById(newid).value="#008000";
+        // }
+        // if (curColor == "blue") {
+        //     document.getElementById(newid).value="#0000FF";
+        // }
+        // if (curColor == "red") {
+        //     document.getElementById(newid).value="#FF0000";
+        // }
+        newid = "d" + id
+        if (curOption == "taken") {
+            change_sub(id, newid, "taken");
+        }
+        else if (curOption == "inprogress") {
+            change_sub(id, newid, "inprogress");
+        }
+        else if (curOption == "future") {
+            change_sub(id, newid, "future");
+        }
+        
+    }
+
+    function change_sub(id, newid, opt) {
+        if (curOption == document.getElementById(newid).value) {
+            document.getElementById(newid).value="";
+            document.getElementById(id).style.backgroundColor = "#ffffff";
+            console.log(document.getElementById(newid).value);
+        }
+        else {
+            document.getElementById(newid).value=curOption;
+            document.getElementById(id).style.backgroundColor = curColor;
+            console.log(document.getElementById(newid).value);
+        }
+    }
