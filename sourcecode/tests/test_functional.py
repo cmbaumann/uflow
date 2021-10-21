db20532e29fc834a4b642b5f0dac934af97e8bd8
Peyton Reed <pdreed@crimson.ua.edu>
2021-10-21 16:44:59 -0500

    testing future semesters

--- sourcecode/tests/test_functional.py
+++ sourcecode/tests/test_functional.py
@@ -17,7 +17,8 @@ def test_register(app, client):
         email=email_string,
         password1="password",
         password2="password",
-        major="Computer Science"
+        major="Computer Science",
+        year="2018"
     ), follow_redirects=True)
     assert b"You are logged in as" in page.data
 
@@ -49,3 +50,52 @@ def test_flowchart_name_fail(app, client):
         flowchartname="test"
     ))
     assert b'You already have a flowchart named test' in page.data
+
+     
+
+def test_future_semesters(app, client):
+    client.post('/login', data=dict(
+        email="flowcharttest@crimson.ua.edu",
+        password="password"
+    ), follow_redirects=True)
+    page = client.post('/flowchart-edit/test', data=dict(
+        name="test",
+        d1="taken",
+        d2="taken",
+        d3="taken",
+        d4="taken",
+        d5="taken",
+        d6="taken",
+        d7="taken",
+        d8="inprogress",
+        d9="inprogress",
+        d10="inprogress",
+        d11="inprogress",
+        d12="inprogress",
+        d13="fall1",
+        d14="fall1",
+        d15="fall1",
+        d16="fall1",
+        d17="fall2",
+        d18="fall2",
+        d19="fall2",
+        d20="fall2",
+        d21="fall2",
+        d22="fall2",
+        d23="spring3",
+        d24="spring3",
+        d25="spring3",
+        d26="spring3",
+        d27="spring3",
+        d28="fall3",
+        d29="fall3",
+        d30="fall3",
+        d31="fall3",
+        d32="fall3",
+        d33="spring4",
+        d34="spring4",
+        d35="spring4",
+        d36="spring4",
+        d37="spring4"
+    ))
+    assert b'Your flowchart has been saved' in page.data
