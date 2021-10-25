c02ba351b2ec7f5e354f2a00783f4c98761c7b55
cmbaumann <55467404+cmbaumann@users.noreply.github.com>
2021-10-25 11:50:57 -0500

    deleted old test cases

--- sourcecode/tests/test_functional.py
+++ sourcecode/tests/test_functional.py
@@ -5,55 +5,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 
-# def test_login(app, client):
-#     page = client.post('/login', data=dict(
-#         email="cmbaumann@crimson.ua.edu",
-#         password="password"
-#     ), follow_redirects=True)
-#     assert b'Hello' in page.data
-
-# def test_register(app, client):
-#     num = random.randint(0, 10000000000)
-#     email_string = str(num) + "@crimson.ua.edu"
-#     page = client.post('/register', data=dict(
-#         firstname="Test",
-#         lastname="Case",
-#         email=email_string,
-#         password1="password",
-#         password2="password",
-#         major="Chemical Engineering"
-#     ), follow_redirects=True)
-#     assert b"Hello You have Logged in as" in page.data
-
-# def test_register_fail(app, client):
-#     page = client.post('/register', data=dict(
-#         firstname="",
-#         lastname="",
-#         email="",
-#         password1="",
-#         password2="",
-#         major="Chemical Engineering"
-#     ), follow_redirects=True)
-#     assert b'Please enter your first name' in page.data
-
-# def test_logout(app, client):
-#         client.post('/login', data=dict(
-#         email="flowcharttest@crimson.ua.edu",
-#         password="password"
-#     ), follow_redirects=True)
-#         page = client.post('/logout', follow_redirects=True)
-#         assert b'You are signed out!' in page.data
-
-# def test_flowchart_name_fail(app, client):
-#     client.post('/login', data=dict(
-#         email="flowcharttest@crimson.ua.edu",
-#         password="password"
-#     ), follow_redirects=True)
-#     page = client.post('/flowchart', data=dict(
-#         flowchartname="test"
-#     ))
-#     assert b'You already have a flowchart named test' in page.data
-
 def test_color(app, client):
     driver = webdriver.Chrome("C:\\Users\\cassi\\Downloads\\chromedriver_win32\\chromedriver.exe")
     driver.get("https://uflow-alabama.herokuapp.com/login")
