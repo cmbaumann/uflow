2923a0d5d72061fd46d656734af79ca041a4fccf
cmbaumann <55467404+cmbaumann@users.noreply.github.com>
2021-11-22 12:20:41 -0600

    chromepath changed

--- sourcecode/tests/test_functional.py
+++ sourcecode/tests/test_functional.py
@@ -5,12 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 # must be the chromedriver file path specific to your machine
-<<<<<<< HEAD
 chromepath = "C:\\Users\\cassi\\Downloads\\chromedriver_win32\\chromedriver.exe"
-=======
-chromepath = "/Users/bradywachs/Downloads/chromedriver" 
->>>>>>> Brady_test3
-
 
 #1
 def test_login(app, client):
