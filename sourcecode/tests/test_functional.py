f1d417d544bdf51e9c8d0d83c948c9ca1277290a
Peyton Reed <pdreed@crimson.ua.edu>
2021-11-22 13:18:41 -0600

    got selenium xpath to work

--- sourcecode/tests/test_functional.py
+++ sourcecode/tests/test_functional.py
@@ -3,6 +3,7 @@ from flask import Flask, request
 from selenium import webdriver 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 # must be the chromedriver file path specific to your machine
 chromepath = "/Users/peytonreed/Downloads/chromedriver"
@@ -157,10 +158,8 @@ def test_completion_percentage(app, client):
     element.click()
     element = driver.find_element_by_id("5")
     element.click()
-    element = driver.findElement(By.xpath("//button[@name='first']"))
+    element = driver.find_element(By.XPATH, "//button[@id='first-btn']")
     element.click()
-    # element = driver.find_element_by_xpath("(//input[@name='btn-primary'])")
-    # element.click()
     assert True
 
 # def test_deselect(app, client):
